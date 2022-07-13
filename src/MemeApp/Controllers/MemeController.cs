24d33a88d825627e5b2445eb3fd3447ee2eb570a
Anders Nyberg <anders.nyberg@alecta.se>
2022-07-13 15:37:29 +0200

    1. Start

---
+++ src/MemeApp/Controllers/MemeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MemeApp.Controllers
+{
+    public class MemeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public MemeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/MemeApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MemeApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
